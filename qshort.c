92e1c3f3ac6fea94237fdf397abb7f8a57d16e31
Keshav Priyadarshi <44315208+keshav-space@users.noreply.github.com>
2021-05-16 16:27:21 +0530

    Update qshort.c

--- qshort.c
+++ qshort.c
@@ -1,45 +1,46 @@
 #include<stdio.h>
-void inputI(int a[],int n){
-    for(int *i=a;i<(a+n);i++)
-        scanf("%d",i);
+
+void inputI(int a[], int n) {
+  for (int * i = a; i < (a + n); i++)
+    scanf("%d", i);
 }
-void printI(int a[],int n){
-    for(int *i=a;i<(a+n);i++)
-        printf("%d ",*i);
-    printf("\n");    
+void printI(int a[], int n) {
+  for (int * i = a; i < (a + n); i++)
+    printf("%d ", * i);
+  printf("\n");
 }
-int partiton(int  a[],int start,int end){
-    printI(a,end);
-    int pindex=start,i,temp;
-    for(i=start;i<end;i++){
-        if(a[i]<a[end]){
-            temp=a[i];
-            a[i]=a[pindex];
-            a[pindex]=temp;
-            pindex++;
-        }
+int partiton(int a[], int start, int end) {
+  printI(a, end);
+  int pindex = start, i, temp;
+  for (i = start; i < end; i++) {
+    if (a[i] < a[end]) {
+      temp = a[i];
+      a[i] = a[pindex];
+      a[pindex] = temp;
+      pindex++;
     }
-    temp=a[pindex];
-    a[pindex]=a[end];
-    a[end]=temp;
-    printI(a,end); 
-    return pindex;   
+  }
+  temp = a[pindex];
+  a[pindex] = a[end];
+  a[end] = temp;
+  printI(a, end);
+  return pindex;
 }
 
-void qshort(int a[],int start,int end){
-    if(start<end){
-        int pivot=partiton(a,start,end);
-        qshort(a,start,pivot-1);
-        qshort(a,pivot+1,end);
-    }
+void qshort(int a[], int start, int end) {
+  if (start < end) {
+    int pivot = partiton(a, start, end);
+    qshort(a, start, pivot - 1);
+    qshort(a, pivot + 1, end);
+  }
 }
 
-int main(){
-    int a[20], n;
-    scanf("%d",&n);
-    inputI(a,n);
-    //printI(a,n);
-    printf("\n");
-    qshort(a,0,n);
-    printI(a,n);
+int main() {
+  int a[20], n;
+  scanf("%d", & n);
+  inputI(a, n);
+  //printI(a,n);
+  printf("\n");
+  qshort(a, 0, n);
+  printI(a, n);
 }
